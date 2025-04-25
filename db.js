6e54378c5596a3255e3405ca063efdc5fd337477
DrSimonovic <dragan.simonovic@ees-bp.edu.me>
2025-04-26 00:12:30 +0200

    Zavrsen navbar

---
+++ db.js
@@ -0,0 +1,62 @@
+let dbCourses = [
+  {
+    title: "Java Script",
+    level: "Beginner",
+    durationHours: 100,
+    instructor: "Danilo Vesovic",
+    modules:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem ab quos facilis non sequi consequatur eius corporis commodi. Libero.",
+    price: 150,
+    certification: true,
+  },
+  {
+    title: "Java Script",
+    level: "Intermediate",
+    durationHours: 100,
+    instructor: "Danilo Vesovic",
+    modules:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem ab quos facilis non sequi consequatur eius corporis commodi. Libero.",
+    price: 300,
+    certification: true,
+  },
+  {
+    title: "Java Script",
+    level: "Advanced",
+    durationHours: 100,
+    instructor: "Danilo Vesovic",
+    modules:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem ab quos facilis non sequi consequatur eius corporis commodi. Libero.",
+    price: 500,
+    certification: true,
+  },
+  {
+    title: "ReactJS",
+    level: "Beginner",
+    durationHours: 100,
+    instructor: "Danilo Vesovic",
+    modules:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem ab quos facilis non sequi consequatur eius corporis commodi. Libero.",
+    price: 200,
+    certification: true,
+  },
+  {
+    title: "PHP",
+    level: "Beginner",
+    durationHours: 100,
+    instructor: "Danilo Vesovic",
+    modules:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem ab quos facilis non sequi consequatur eius corporis commodi. Libero.",
+    price: 200,
+    certification: true,
+  },
+  {
+    title: "Laravel",
+    level: "Beginner",
+    durationHours: 100,
+    instructor: "Danilo Vesovic",
+    modules:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem ab quos facilis non sequi consequatur eius corporis commodi. Libero.",
+    price: 500,
+    certification: true,
+  },
+];
