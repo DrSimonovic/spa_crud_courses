562fb22bd4ab6ca2d0998c8242a09419259021bc
DrSimonovic <dragan.simonovic@ees-bp.edu.me>
2025-04-29 13:46:40 +0200

    delete

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 let dbCourses = [];
+// localStorage.db = "";
 if (localStorage.db && JSON.parse(localStorage.db).length > 0) {
   dbCourses = JSON.parse(localStorage.db);
 } else {
   dbCourses = [
     {
+      id: 1,
       title: "Java Script",
       level: "Beginner",
       duration: 100,
@@ -12,44 +14,49 @@ if (localStorage.db && JSON.parse(localStorage.db).length > 0) {
       icon: "https://raw.githubusercontent.com/devicons/devicon/ca28c779441053191ff11710fe24a9e6c23690d6/icons/javascript/javascript-original.svg",
     },
     {
-      title: "Java Script",
+      id:2,
+      title: "CSS 3",
       level: "Intermediate",
       duration: 150,
       instructor: "Bogdan Bogdanovic",
       price: 300,
-      icon: "pictures\javascript-original.svg",
+      icon: "https://raw.githubusercontent.com/devicons/devicon/ca28c779441053191ff11710fe24a9e6c23690d6/icons/css3/css3-original.svg",
     },
     {
+      id:3,
       title: "Java Script",
       level: "Advanced",
       duration: 100,
       instructor: "Petar Petrovic",
       price: 500,
-      icon: "pictures\javascript-original.svg",
+      icon: "pictures/javascript-original.svg",
     },
     {
+      id:4,
       title: "ReactJS",
       level: "Beginner",
       duration: 80,
       instructor: "Branimir Stulic",
       price: 200,
-      icon: "pictures\react-original.svg",
+      icon: "pictures/react-original.svg",
     },
     {
+      id:5,
       title: "PHP",
       level: "Beginner",
       duration: 110,
       instructor: "Kawhi Leonard",
       price: 200,
-      icon: "pictures\php-original.svg",
+      icon: "pictures/php-original.svg",
     },
     {
+      id:6,
       title: "Laravel",
       level: "Beginner",
       duration: 300,
       instructor: "Jimmy Butler",
       price: 500,
-      icon: "pictures\react-original.svg",
+      icon: "https://raw.githubusercontent.com/devicons/devicon/ca28c779441053191ff11710fe24a9e6c23690d6/icons/laravel/laravel-original.svg",
     },
   ];
 }
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let coursesContainer = document.querySelector(".container");
-
 // views
 
 let coursesView = document.querySelector("#courses-view");
@@ -17,12 +15,15 @@ let btnSaveInput = document.querySelector(".btn-save");
 
 // inputs
 
-let titleInput = document.getElementsByName("title")[0];
-let levelInput = document.getElementsByName("level")[0];
-let durationInput = document.getElementsByName("duration")[0];
-let instructorInput = document.getElementsByName("instructor")[0];
-let priceInput = document.getElementsByName("price")[0];
-let iconInput = document.getElementsByName("icon")[0];
+let titleInput = document.querySelector("#title-input-add");
+let levelInput = document.querySelector("#level-input-add");
+let durationInput = document.querySelector("#duration-input-add");
+let instructorInput = document.querySelector("#instructor-input-add");
+let priceInput = document.querySelector("#price-input-add");
+let iconInput = document.querySelector("#icon-input-add");
+
+let coursesContainer = coursesView.querySelector(".container");
+let editDeleteContainer = editDeleteView.querySelector(".container");
 
 // listeners
 
@@ -45,6 +46,7 @@ function displayCoursesView(e) {
   addView.style.display = "none";
   editDeleteView.style.display = "none";
   linksView.style.display = "none";
+  createCourses();
   coursesView.style.display = "block";
 }
 
@@ -65,6 +67,7 @@ function displayEditDeleteView(e) {
   coursesView.style.display = "none";
   addView.style.display = "none";
   linksView.style.display = "none";
+  createEditDeleteCards();
   editDeleteView.style.display = "block";
 }
 
@@ -83,24 +86,34 @@ function createNewCourse(e) {
     e.preventDefault();
   }
   let newCourse = {
+    id: dbCourses.length > 0 ? dbCourses[dbCourses.length - 1].id + 1 : 1, // id novog kursa za 1 veci od id-ja zadnjeg
     title: titleInput.value,
     level: levelInput.value,
     duration: durationInput.value,
     instructor: instructorInput.value,
     price: priceInput.value,
-    icon: iconInput.value,
+    icon: iconInput.value + `?${Math.random()}`, // sprjecava kesiranje
   };
   dbCourses[dbCourses.length] = newCourse;
   resetInputs();
   createCourses();
 }
 
+function deleteCourse(e) {
+  if (e) {
+    e.preventDefault();
+  }
+  dbCourses.splice(this.dataset.id, 1);
+  createCourses();
+  displayCoursesView();
+}
+
 // end event listeners
 
-createCourses();
+displayCoursesView();
 
 function createCourses() {
-  html = "";
+  let html = "";
   dbCourses.forEach((course) => {
     html += `<div class="card">
             <div class="row">
@@ -117,9 +130,43 @@ function createCourses() {
           </div>`.trim();
   });
   coursesContainer.innerHTML = html;
-  displayCoursesView();
-  // dbCourses.splice(dbCourses.length-1, 1);
-  console.log(dbCourses);
+  // displayCoursesView();
+}
+
+function createEditDeleteCards() {
+  let html = "";
+  dbCourses.forEach((course, index) => {
+    html += `<div class="card">
+            <div class="row">
+              <div class="level">${course.level}</div>
+              <div class="duration">${course.duration} hours</div>
+            </div>
+            <img
+              src="${course.icon}"
+              alt=""
+            />
+            <div class="title">${course.title}</div>
+            <div class="instructor">${course.instructor}</div>
+            <div class="price">${course.price} EUR</div>
+            <div class="row-btns">
+              <button data-id="${index}" class="btn btn-edit">Edit</button>
+              <button data-id="${index}" class="btn btn-delete">Delete</button>
+            </div>
+          </div>`.trim();
+  });
+  editDeleteContainer.innerHTML = html;
+
+  let btnDeleteAll = editDeleteContainer.querySelectorAll(".btn-delete");
+  let btnEditAll = editDeleteContainer.querySelectorAll(".btn-edit");
+  btnDeleteAll.forEach((btnDelete, index) => {
+    btnDelete.addEventListener("click", deleteCourse);
+    btnEditAll[index].addEventListener("click", showCourse);
+  });
+}
+
+function showCourse(){
+  console.log(dbCourses[this.dataset.id].id)
+  // alert(`Radi dugme edit na kursu  ${dbCourses[this.dataset.id].name} `);
 }
 
 function resetInputs() {
